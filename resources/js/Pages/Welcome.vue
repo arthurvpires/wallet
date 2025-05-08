4cdcd22d2d2d94a0f7f3001ed732e8f9ff723b75
Arthur <arthurvpires@live.com>
2025-05-08 11:32:03 -0300

    Improve Welcome view

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -7,39 +7,25 @@ defineProps({
     },
     canRegister: {
         type: Boolean,
-    },
-    laravelVersion: {
-        type: String,
-        required: true,
-    },
-    phpVersion: {
-        type: String,
-        required: true,
-    },
+    }
 });
 
-function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
-}
 </script>
 
 <template>
     <Head title="Welcome" />
-    <div class="bg-[#6a97ca] min-h-screen flex items-center justify-center">
-        <div class="text-center">
-            <h1 class="text-4xl font-bold text-white mb-8">Wallet App</h1>
+    <div class="bg-[#ffffff] min-h-screen flex items-center justify-center">
+        <div class="text-center bg-[#c1daeb] w-1/4  rounded-xl shadow-lg p-10">
+            <h1 class="text-4xl font-bold text-blue-900 mb-8">Wallet App</h1>
             <div class="space-y-4">
                 <Link v-if="canLogin" :href="route('login')"
                     class="block bg-white text-blue-900 font-semibold px-6 py-3 rounded-lg hover:bg-blue-50 transition-colors">
-                    Login
+                Login
                 </Link>
 
                 <Link v-if="canRegister" :href="route('register')"
                     class="block bg-blue-800 text-white font-semibold px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors">
-                    Cadastrar-se
+                Cadastrar-se
                 </Link>
             </div>
         </div>
